28b1126fa25a3fc174e2630526b86eaa0ba38b6b
leomono35 <55753054+leomono35@users.noreply.github.com>
2019-12-10 17:10:01 +0100

    Create topologicalSort.h

---
+++ topologicalSort.h
@@ -0,0 +1,7 @@
+#ifndef TOPOLOGICAL_SORTING_H_INCLUDED
+#define TOPOLOGICAL_SORTING_H_INCLUDED
+
+void topologicalSortUtil(int id, Graph& G, stack<int> &Stack);
+void topologicalSort(Graph& G);
+
+#endif // TOPOLOGICAL_SORTING_H_INCLUDED
