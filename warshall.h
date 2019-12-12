6de84e3d8aabf85388258ce9d52c7051013b57d1
leomono35 <55753054+leomono35@users.noreply.github.com>
2019-12-12 19:44:30 +0100

    Create warshall.h

---
+++ warshall.h
@@ -0,0 +1,7 @@
+#ifndef WARSHALL_H_INCLUDED
+#define WARSHALL_H_INCLUDED
+
+void printSolution(int dist[][G.number_of_vertex]);
+void floydWarshall (int standardTab[]);
+
+#endif // WARSHALL_H_INCLUDED
